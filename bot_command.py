90e873c59d57a32985e5730ff9f41944072b190e
Alice Shu <alice@e26.ru>
2022-12-22 14:08:59 +0800

    готовность 2

--- bot_command.py
+++ bot_command.py
@@ -1,25 +1,6 @@
-from telegram import Update
+from telebot
+from telebot import types
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import datetime
 from spy import *
 
-async def hi_com(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    log(update, context)
-    await update.message.reply_text(f'Hello  {update.effective_user.first_name}')
 
-async def time_com(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
-    await update.message.reply_text(f'{datetime.datetime.now().time()}')
-
-async def help_com(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
-    await update.message.reply_text(f'/hi\n/time\n/help')
-
-async def sum_com(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
-    msg = update.message.text
-    
-    items = msg.split()
-    x = int(items[1])
-    y = int(items[2])
-    await update.message.reply_text(f'{x} + {y} = {x+y}')
